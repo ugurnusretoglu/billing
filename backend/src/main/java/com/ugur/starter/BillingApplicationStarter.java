1d38e4ffb5aec4ab12da0b9590ec9db8ac39ffd2
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-09-16 23:39:02 +0300

    Initial commit: backend and frontend initialized

---
+++ backend/src/main/java/com/ugur/starter/BillingApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.ugur.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingApplicationStarter.class, args);
+	}
+
+}
